d86c72809edde46b04f9b2b7bc01239b458110d6
Omar Awile <omar.awile@epfl.ch>
2018-04-23 17:57:26 +0200

    added a simple openmp version to measure bandwidth when multiple cores are reading/writing

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 CXX=icpc
-CXXFLAGS=-O3 -qopt-zmm-usage=high -qopt-report=5 -xCORE-AVX512 -std=c++11
+CXXFLAGS=-O3 -qopt-zmm-usage=high -qopt-report=5 -xCORE-AVX512 -std=c++11 -qopenmp
 
 
 cbench: cache_bench.cpp
--- cache_bench.cpp
+++ cache_bench.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <stdlib.h>
-
+#include <omp.h>
 
 
 int main(int argc, char* argv[]) {
@@ -12,41 +12,64 @@ int main(int argc, char* argv[]) {
   int increment = 1024/sizeof(float);
 
   int niter = 100000;
-  
-  for (int sz = minN; sz < maxN; sz+=increment) {
+  int nthreads = omp_get_max_threads();
 
-    float *const a_arr = (float *const) _mm_malloc(sz*sizeof(float), 64);
-    float *const b_arr = (float *const) _mm_malloc(sz*sizeof(float), 64);
-    
-    for (int i = 0; i < sz; ++i) {
+  float **global_a = (float**) malloc(nthreads*sizeof(float*));
+  float **global_b = (float**) malloc(nthreads*sizeof(float*));
+ 
+  for (int ithr = 0; ithr < nthreads; ++ithr) {
+    float *const a_arr = (float *const) _mm_malloc(maxN*sizeof(float), 64);
+    float *const b_arr = (float *const) _mm_malloc(maxN*sizeof(float), 64);
+    global_a[ithr] = a_arr; 
+    global_b[ithr] = b_arr; 
+  }
+
+#pragma omp parallel for schedule(static,1)
+  for (int ithr = 0; ithr < nthreads; ++ithr) {
+    auto a_arr = global_a[ithr]; 
+    auto b_arr = global_b[ithr]; 
+    for (int i = 0; i < maxN; ++i) {
       a_arr[i] = 1.0f;
       b_arr[i] = 2.0f;
     }
-    
-    float ab_dot = 0.0f;
-    // warmup
-    for (int iter = 0; iter < 100; ++iter) {
-      for (int i = 0; i < sz; ++i) {
-        ab_dot += a_arr[i]*b_arr[i];
-      }
-    }
+  }
+ 
 
-    auto tstart = std::chrono::high_resolution_clock::now();
-    long int btick = __rdtsc(); 
-    for (int iter = 0; iter < niter; ++iter) {
-      #pragma unroll(16)
-      for (int i = 0; i < sz; ++i) {
-        ab_dot += a_arr[i]*b_arr[i];
-      }
-    }
-    long int etick = __rdtsc(); 
-    auto tend = std::chrono::high_resolution_clock::now();
-    double bytes = (2.0*niter*sz*sizeof(float));
-    double bperc = bytes/(etick - btick);
-    std::chrono::duration<double> tdiff = tend - tstart;
+  for (int sz = minN; sz < maxN; sz+=increment) {
     
-    std::cout << 2*sz*sizeof(float) << ", " << bperc << ", " 
-      << (bytes/1e9)/tdiff.count() << ", " << ab_dot << std::endl;
+#pragma omp parallel for schedule(static,1)
+    for (int ithr = 0; ithr < nthreads; ++ithr) {
+      auto a_arr = global_a[ithr]; 
+      auto b_arr = global_b[ithr]; 
+      
+      
+      
+      float ab_dot = 0.0f;
+      // warmup
+      for (int iter = 0; iter < 100; ++iter) {
+        for (int i = 0; i < sz; ++i) {
+          ab_dot += a_arr[i]*b_arr[i];
+        }
+      }
+
+      auto tstart = std::chrono::high_resolution_clock::now();
+      long int btick = __rdtsc(); 
+      for (int iter = 0; iter < niter; ++iter) {
+        #pragma unroll(16)
+        for (int i = 0; i < sz; ++i) {
+          ab_dot += a_arr[i]*b_arr[i];
+        }
+      }
+      long int etick = __rdtsc(); 
+      auto tend = std::chrono::high_resolution_clock::now();
+      double bytes = (2.0*niter*sz*sizeof(float));
+      double bperc = bytes/(etick - btick);
+      std::chrono::duration<double> tdiff = tend - tstart;
+     
+#pragma omp critical
+      std::cout << omp_get_thread_num() << ": " << 2*sz*sizeof(float) << ", " << bperc << ", " 
+        << (bytes/1e9)/tdiff.count() << ", " << ab_dot << std::endl;
+    }
   }
     
 
